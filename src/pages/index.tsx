f0a3409eaa689fbdfd7f51e8af823c20290b8fa2
yutteee <yusaku_nakamura_aa@icloud.com>
2024-03-20 14:30:06 +0900

    css変数の作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ export default function Home() {
       </Head>
       <main>
         <h1>Home</h1>
+        <div>日本語の文字ですよろしく。</div>
         <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
       </main>
     </>
