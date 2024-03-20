5e3f3535eae449e39e743a58d989b334338f8f34
yutteee <yusaku_nakamura_aa@icloud.com>
2024-03-20 18:31:19 +0900

    各ページへの設置

--- src/ui/Header/index.tsx
+++ src/ui/Header/index.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import { IoMdMenu } from "react-icons/io";
 import styles from "./index.module.scss";
 
 type Props = {};
@@ -13,20 +14,23 @@ export const Header: React.FC<Props> = (props) => {
       <div className={styles.right}>
         <nav className={styles.nav}>
           <Link href="/products">プロダクト</Link>
-          <Link href="/member">メンバー</Link>
+          <Link href="/members">メンバー</Link>
           <Link href="/activities">活動内容</Link>
           <Link href="/achievements">活動実績</Link>
           <Link href="/faq">FAQ</Link>
           <Link href="/blog">ブログ</Link>
         </nav>
         <Link
-          href="https://jack-website.netlify.app/"
-          about="_blank"
+          href="https://docs.google.com/forms/d/e/1FAIpQLSfOj8Twb_KlxPEr2whaQu2POouv_uFSJ27qUTc5cMWKEzxETw/viewform"
+          target="_blank"
           rel="noopener noreferrer"
           className={styles.contact}
         >
           体験応募はこちら
         </Link>
+        <button className={styles.menuButton}>
+          <IoMdMenu size={24} />
+        </button>
       </div>
     </header>
   );
