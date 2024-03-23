b1d96466bdbc861e6805088ee0f5fb04c93982a0
Sattsunn <satsusarii@gmail.com>
2024-03-23 14:44:06 +0900

    見た目

--- src/ui/MemberStory/index.tsx
+++ src/ui/MemberStory/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import styles from "./index.module.scss";
 import { BiPhotoAlbum } from "react-icons/bi";
 import image from "next/image";
+import { type } from "os";
 
 type Props = {
+  image: string;
   name: string;
   text: string;
+  type: string;
 };
 
-export const MemberStory: React.FC<Props> = ({ name, text }) => {
+export const MemberStory: React.FC<Props> = ({ name, text, type, image }) => {
   return (
     <>
       <div className={styles.wrapper}>
@@ -19,11 +22,11 @@ export const MemberStory: React.FC<Props> = ({ name, text }) => {
         <div className={styles.card}>
           <div className={styles.card__header}>
             {" "}
-            <div className={styles.name}>hogehoge</div>
-            <div className={styles.type}>名古屋大学</div>
+            <div className={styles.name}>{name}</div>
+            <div className={styles.type}>{type}</div>
           </div>
 
-          <div className={styles.text}>aiueo</div>
+          <div className={styles.text}>{text}</div>
         </div>
       </div>
     </>
