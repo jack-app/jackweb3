67a609381a63b7cae8cab7d1bb1ab726f5d2ce29
yutteee <yusaku_nakamura_aa@icloud.com>
2024-04-01 21:52:18 +0900

    storybookの微調整 #30

--- src/ui/Accordion/index.stories.tsx
+++ src/ui/Accordion/index.stories.tsx
@@ -1,15 +1,15 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { Accordion } from './';
+import type { Meta, StoryObj } from "@storybook/react";
+import { Accordion } from "./";
 
 type T = typeof Accordion;
 
 const meta: Meta<T> = {
-  title: 'ui/Accordion',
+  title: "ui/Accordion",
   component: Accordion,
   parameters: {
-    layout: 'centered',
+    controls: { expanded: true },
   },
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   argTypes: { question: { control: "text" }, answer: { control: "text" } },
 };
 
@@ -19,6 +19,6 @@ type Story = StoryObj<T>;
 export const Default: Story = {
   args: {
     question: "question",
-    answer: "answer"
+    answer: "answer",
   },
 };
