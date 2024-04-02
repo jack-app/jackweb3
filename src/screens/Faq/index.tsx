36cc2be53b08aafa0c3aacf819de6c1c0601ad0f
yutteee <yusaku_nakamura_aa@icloud.com>
2024-04-02 13:52:11 +0900

    FAQ screen #32

---
+++ src/screens/Faq/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+type Props = {};
+
+export const FaqScreen: React.FC<Props> = (props) => {
+  return (
+    <div>
+      <h1>Faqページです。</h1>
+    </div>
+  );
+};
