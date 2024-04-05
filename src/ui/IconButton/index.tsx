00766dd8278eb2c6e4230690e538153aa4f286d2
Sattsunn <satsusarii@gmail.com>
2024-04-05 17:20:05 +0900

    IconButtonアイコンとリンクの渡し方変更

--- src/ui/IconButton/index.tsx
+++ src/ui/IconButton/index.tsx
@@ -1,25 +1,27 @@
-import Image from "next/image";
 import React from "react";
 import styles from "./index.module.scss";
+import { IconType } from "react-icons";
+import Link from "next/link";
 
 type Props = {
+  href: string;
   text: string;
-  imgUrl: string;
-  onClick: () => void;
+  icon: IconType;
   size: "s" | "l";
 };
 
-export const IconButton: React.FC<Props> = ({ text, imgUrl, onClick, size }) => {
+export const IconButton: React.FC<Props> = ({ href, icon: Icon, text, size }) => {
   let btnClassName = `${styles.btn} `;
   if (size === "s") {
     btnClassName += `${styles.small}`;
   } else {
     btnClassName += `${styles.large}`;
   }
+
   return (
-    <button className={btnClassName} onClick={onClick}>
+    <Link href={href} className={btnClassName}>
       <span className={styles.text}>{text}</span>
-      <Image src={imgUrl} alt="icon" className={styles.icon} width={24} height={24} />
-    </button>
+      <Icon className={styles.icon} width={24} height={24} />
+    </Link>
   );
 };
