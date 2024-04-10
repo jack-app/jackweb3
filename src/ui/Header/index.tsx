3500f169f57e4d382a71a786dfe88570817bbc67
shioneko <shioneko37@gmail.com>
2024-04-10 10:27:02 +0900

    button機能 #21

--- src/ui/Header/index.tsx
+++ src/ui/Header/index.tsx
@@ -1,9 +1,8 @@
 import Link from "next/link";
 import React from "react";
 import { IoMdMenu } from "react-icons/io";
-import styles from "./index.module.scss";
-import { useState } from "react";
 import { IoClose } from "react-icons/io5";
+import styles from "./index.module.scss";
 
 type Props = {};
 
@@ -14,13 +13,13 @@ export const Header: React.FC<Props> = (props) => {
         jack
       </Link>
       <div className={styles.right}>
-        <nav className={styles.nav} >
+        <nav className={styles.nav}>
           <Link href="/products">プロダクト</Link>
-          <Link href="/Members" >メンバー</Link>
-          <Link href="/Activities" >活動内容</Link>
+          <Link href="/Members">メンバー</Link>
+          <Link href="/Activities">活動内容</Link>
           <Link href="/Achievements">活動実績</Link>
           <Link href="/FAQ">FAQ</Link>
-          <Link href="/blog" >ブログ</Link>
+          <Link href="/blog">ブログ</Link>
         </nav>
         <Link
           href="https://docs.google.com/forms/d/e/1FAIpQLSfOj8Twb_KlxPEr2whaQu2POouv_uFSJ27qUTc5cMWKEzxETw/viewform"
@@ -32,35 +31,30 @@ export const Header: React.FC<Props> = (props) => {
         </Link>
         <div className={styles.hamburger_menu}>
           <input type="checkbox" id={styles.check1} className={styles.menu_btn_check} />
-          <label htmlFor={styles.check1} className={styles.menubtn}><span className={styles.a}>a</span>
-            {/* <IoMdMenu size={24} className={styles.hamburger_btn} /> */}
+          <label htmlFor={styles.check1} className={styles.menubtn}>
+            <span className={styles.icon}></span>
           </label>
-          {/* <input type="checkbox" id={styles.check2} className={styles.menu_btn_check} />
-          <label htmlFor={styles.check2} className={styles.menubtn}>
-            <IoClose size={24} className={styles.close_btn} />
-          </label> */}
-          <div className={styles.ham_btn}>
-            <IoMdMenu size={24} className={styles.hamburger_btn} />
+          <div className={styles.hamburger_btn}>
+            <IoMdMenu size={24} />
           </div>
-          <div className={styles.clo_btn}>
-            <IoClose size={24} className={styles.close_btn} />
+          <div className={styles.close_btn}>
+            <IoClose size={24} />
           </div>
           <div className={styles.menu}>
-            <button className={styles.black} ></button>
+            <button className={styles.black}></button>
             <div className={styles.menubar}>
-              <nav className={styles.hamburgerNav} >
+              <nav className={styles.hamburgerNav}>
                 <Link href="/products">プロダクト</Link>
-                <Link href="/Members" >メンバー</Link>
-                <Link href="/Activities" >活動内容</Link>
+                <Link href="/Members">メンバー</Link>
+                <Link href="/Activities">活動内容</Link>
                 <Link href="/Achievements">活動実績</Link>
                 <Link href="/FAQ">FAQ</Link>
-                <Link href="/blog" >ブログ</Link>
+                <Link href="/blog">ブログ</Link>
               </nav>
             </div>
           </div>
         </div>
       </div>
-
     </header>
   );
 };
