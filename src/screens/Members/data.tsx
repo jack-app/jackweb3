cdf1aa34e6bcf35a26b73e9c0d93291998eaa91a
kotttto <nanasan770@outlook.com>
2024-04-14 21:29:55 +0900

    メンバー紹介画面の実装（途中経過）

---
+++ src/screens/Members/data.tsx
@@ -0,0 +1,19 @@
+import next from "next";
+import Image from "next/image";
+import React, { useState } from "react";
+import { MemberStory } from "@/ui/MemberStory/index";
+
+let array1 = [
+  ["名前", "文", "タイプ", "写真"],
+  ["", "", "", ""],
+  ["", "", "", ""],
+];
+
+for (let i = 0; i < array1.length; i++) {
+  let array2 = array1[i];
+
+  let name = array2[0];
+  let text = array2[1];
+  let type = array2[2];
+  let image = array2[3];
+}
--- src/screens/Members/index.tsx
+++ src/screens/Members/index.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import styles from './index.module.scss';
+import next from "next";
+import Image from "next/image";
+import React, { useState } from "react";
+import { MemberStory } from "@/ui/MemberStory/index";
+import styles from "./index.module.scss";
 
 type Props = {};
 
 export const MembersScreen: React.FC<Props> = (props) => {
+  const [] = useState("");
   return (
-    <div>
-      <h1>Membersページです。</h1>
-    </div>
+    <>
+      <div>
+        <h1 className={styles.allnum}>全体人数：約70人</h1>
+        <h2 className={styles.mf}>男女比</h2>
+        <h2 className={styles.univ}>大学</h2>
+        <h2 className={styles.dep}>学部</h2>
+        <Image className={styles.mf_photo} src="/mf_photo.png" alt="" width={200} height={200} />
+        <Image
+          className={styles.univ_photo}
+          src="/univ_photo.png"
+          alt=""
+          width={200}
+          height={200}
+        />
+        <p>他大学：</p>
+        <Image className={styles.dep_photo} src="/dep_photo.png" alt="" width={200} height={200} />
+        <p>その他：理学部、理工学部、法学部、文学部、教育学部、etc…</p>
+      </div>
+
+      <div>
+        <h1 className={styles.memberstory}>メンバーのお話</h1>
+      </div>
+    </>
   );
 };
