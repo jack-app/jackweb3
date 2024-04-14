aa982e3878a66ff266521049c6682b03fa2823aa
ai8nik <ai8nik@gmail.com>
2024-04-14 22:07:47 +0900

    タイムライン作った

---
+++ src/ui/Timeline/index.tsx
@@ -0,0 +1,59 @@
+// import React from "react";
+// import styles from "./index.module.scss";
+import React from "react";
+import styles from "./index.module.scss";
+
+type Props = {};
+
+export const Timeline: React.FC<Props> = (props) => {
+  return (
+    <div>
+      <ul className={styles.timeline}>
+        <li>
+          <div className={styles.timeline_content}>
+            <img src="https://placehold.jp/360x200.png"></img>
+            <h3>4月 新歓</h3>
+            <p>
+              jackメンバーが作成したプロダクトを新入生のみなさんに紹介します。体験もできるかも…？！
+            </p>
+            <div className={styles.bar}></div>
+          </div>
+        </li>
+        <li>
+          <div className={styles.timeline_content}>
+            <img src="https://placehold.jp/360x200.png"></img>
+            <h3>5月 jack hack</h3>
+            <p>2~3日でチームでアプリを開発してみよう。君のアプリは一番人気を勝ち取れるか！？</p>
+            <div className={styles.bar}></div>
+          </div>
+        </li>
+        <li>
+          <div className={styles.timeline_content}>
+            <img src="https://placehold.jp/360x200.png"></img>
+            <h3>6月 名大祭</h3>
+            <p>チームで制作したアプリやゲームを展示します。実際にお客さんに遊んでもらおう！</p>
+            <div className={styles.bar}></div>
+          </div>
+        </li>
+        <li>
+          <div className={styles.timeline_content}>
+            <img src="https://placehold.jp/360x200.png"></img>
+            <h3>8月 アイデアソン</h3>
+            <p>
+              チームで協力して、お題に沿った新しいアプリのアイデアを出し合おう。君のアイデアを待ってるよ！
+            </p>
+            <div className={styles.bar}></div>
+          </div>
+        </li>
+        <li>
+          <div className={styles.timeline_content}>
+            <img src="https://placehold.jp/360x200.png"></img>
+            <h3>12月 jack fes</h3>
+            <p>さぁ、一年の集大成を発表しよう！努力の証をここに残そう。</p>
+            <div className={styles.bar}></div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
