69107e044f9743adbb46c280dfadc098dbb3d8ca
shioneko <shioneko37@gmail.com>
2024-04-22 12:22:17 +0900

    一旦push

--- src/screens/Achievements/index.tsx
+++ src/screens/Achievements/index.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import styles from './index.module.scss';
+import React from "react";
+import styles from "./index.module.scss";
+import { Heading1 } from "@/ui/Heading1";
+import { Heading2 } from "@/ui/Heading2";
 
 type Props = {};
 
 export const AchievementsScreen: React.FC<Props> = (props) => {
   return (
-    <div>
-      <h1>Achievementsページです。</h1>
-    </div>
+    <main>
+      <Heading1 enTitle="Achievements" jaTitle="活動実績" />
+      <div className={styles.years}>
+        <Heading2 text="2023" />
+        <div className={styles.content}></div>
+      </div>
+      <div className={styles.years}>
+        <Heading2 text="2022" />
+        <div className={styles.content}></div>
+      </div>
+      <div className={styles.years}>
+        <Heading2 text="2020" />
+        <div className={styles.content}></div>
+      </div>
+      <div className={styles.articlesWrapper}></div>
+    </main>
   );
 };
