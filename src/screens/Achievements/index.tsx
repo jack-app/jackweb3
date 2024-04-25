62c002c0a7a94af6356f43402101457b69379ef9
shioneko <shioneko37@gmail.com>
2024-04-25 17:12:06 +0900

    一旦commit

--- src/screens/Achievements/index.tsx
+++ src/screens/Achievements/index.tsx
@@ -1,27 +1,43 @@
 import React from "react";
+import { y2023 } from "./data";
 import styles from "./index.module.scss";
+import { AchievementItem } from "@/ui/AchievementItem";
 import { Heading1 } from "@/ui/Heading1";
 import { Heading2 } from "@/ui/Heading2";
-
 type Props = {};
 
 export const AchievementsScreen: React.FC<Props> = (props) => {
   return (
     <main>
       <Heading1 enTitle="Achievements" jaTitle="活動実績" />
-      <div className={styles.years}>
-        <Heading2 text="2023" />
-        <div className={styles.content}></div>
+      <div className={styles.achievements}>
+        <div className={styles.years}>
+          <Heading2 text="2023" />
+          <div className={styles.content}>
+            {y2023.map((prop, num) => (
+              <AchievementItem
+                key={num}
+                image={prop.image}
+                date={prop.date}
+                text={prop.text}
+                article_href={prop.article_href}
+                web_href={prop.web_href}
+                app_href={prop.article_href}
+                google_href={prop.article_href}
+                git_href={prop.article_href}
+              />
+            ))}
+          </div>
+        </div>
+        <div className={styles.years}>
+          <Heading2 text="2022" />
+          <div className={styles.content}></div>
+        </div>
+        <div className={styles.years}>
+          <Heading2 text="2020" />
+          <div className={styles.content}></div>
+        </div>
       </div>
-      <div className={styles.years}>
-        <Heading2 text="2022" />
-        <div className={styles.content}></div>
-      </div>
-      <div className={styles.years}>
-        <Heading2 text="2020" />
-        <div className={styles.content}></div>
-      </div>
-      <div className={styles.articlesWrapper}></div>
     </main>
   );
 };
