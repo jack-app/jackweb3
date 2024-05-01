2813ba932e0fe3ecbbd43bbb65f669894c36c7e2
yutteee <yusaku_nakamura_aa@icloud.com>
2024-05-01 22:05:09 +0900

    Heading1追加などの微調整 #35

--- src/screens/Members/data.ts
+++ src/screens/Members/data.ts
@@ -1,5 +1,3 @@
-import { title } from "process";
-
 export const memberStories = [
   {
     name: "すけ",
--- src/screens/Members/index.tsx
+++ src/screens/Members/index.tsx
@@ -1,44 +1,39 @@
 import Image from "next/image";
-import { Props } from "next/script";
 import React from "react";
+import { Heading1 } from "@/ui/Heading1";
 import { Heading2 } from "@/ui/Heading2";
 import { MemberStory } from "@/ui/MemberStory/index";
 import { Members, memberStories } from "./data";
 import styles from "./index.module.scss";
 
-export const MembersScreen: React.FC<Props> = () => {
+export const MembersScreen: React.FC = () => {
   return (
-    <>
+    <main>
+      <Heading1 enTitle="Members" jaTitle="メンバー" />
       <div className={styles.wrapper}>
         <h1 className={styles.allnum}>
           全体人数：約<span>70</span>人
         </h1>
         <div className={styles.graph}>
-          {Members.map((Mem) => {
-            return (
-              <>
-                <div className={styles.graphcontent}>
-                  <h2 className={styles.title}>{Mem.title}</h2>
-                  <Image
-                    className={styles.malefemale_graph}
-                    src={Mem.graph}
-                    alt=""
-                    width={300}
-                    height={300}
-                  />
-                  <p className={styles.detail}>{Mem.detail}</p>
-                </div>
-              </>
-            );
-          })}
+          {Members.map((member, index) => (
+            <div className={styles.graphcontent} key={index}>
+              <h2 className={styles.title}>{member.title}</h2>
+              <Image
+                className={styles.malefemale_graph}
+                src={member.graph}
+                alt=""
+                width={300}
+                height={300}
+              />
+              <p className={styles.detail}>{member.detail}</p>
+            </div>
+          ))}
         </div>
-
         <div className={styles.memberstory}>
           <Heading2 text="メンバーのお話" />
-
-          {memberStories.map((prop, num) => (
+          {memberStories.map((prop, index) => (
             <MemberStory
-              key={num}
+              key={index}
               name={prop.name}
               text={prop.text}
               type={prop.type}
@@ -47,6 +42,6 @@ export const MembersScreen: React.FC<Props> = () => {
           ))}
         </div>
       </div>
-    </>
+    </main>
   );
 };
