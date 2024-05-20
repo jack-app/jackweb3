a63b42c3c0248348e503a6347594fd1a8dcbff68
shioneko <shioneko37@gmail.com>
2024-05-20 17:17:51 +0900

    活動実績年ごとに表示 #52

--- src/screens/Achievements/index.tsx
+++ src/screens/Achievements/index.tsx
@@ -3,31 +3,31 @@ import styles from "./index.module.scss";
 import { AchievementItem, Props as AchievementItemProps } from "@/ui/AchievementItem";
 import { Heading1 } from "@/ui/Heading1";
 import { Heading2 } from "@/ui/Heading2";
+import { groupBy } from "@/utils/groupBy";
 
 type Props = {
   achievements: AchievementItemProps[];
 };
 export const AchievementsScreen: React.FC<Props> = ({ achievements }) => {
+  const groupedAchievement = groupBy(achievements, (achievement) => {
+    return new Date(achievement.date).getFullYear().toString();
+  });
+
+  const sortedYear = Object.keys(groupedAchievement).sort((a, b) => (a > b ? -1 : 1));
   return (
     <main>
       <Heading1 enTitle="Achievements" jaTitle="活動実績" />
-      <div className={styles.achievements}>
-        <div className={styles.years}>
-          <Heading2 text="2023" />
-          <div className={styles.content}></div>
-        </div>
-        <div className={styles.years}>
-          <Heading2 text="2022" />
-          <div className={styles.content}>
-            {achievements.map((achievement) => (
-              <AchievementItem key={achievement.id} {...achievement} />
-            ))}
+      <div className={styles.wrapper}>
+        {sortedYear.map((year) => (
+          <div key={year} className={styles.content}>
+            <Heading2 text={year} />
+            <div className={styles.content}>
+              {groupedAchievement[year].map((achievement) => (
+                <AchievementItem key={achievement.id} {...achievement} />
+              ))}
+            </div>
           </div>
-        </div>
-        <div className={styles.years}>
-          <Heading2 text="2020" />
-          <div className={styles.content}></div>
-        </div>
+        ))}
       </div>
     </main>
   );
