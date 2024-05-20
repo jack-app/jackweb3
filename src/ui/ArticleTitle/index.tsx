e563d3bd6d4b9b7470de89d2aaa7cc7b6fab61e0
Riku-0801 <riku20020801@gmail.com>
2024-05-20 13:13:53 +0900

    fix: comment

--- src/ui/ArticleTitle/index.tsx
+++ src/ui/ArticleTitle/index.tsx
@@ -1,21 +1,14 @@
 import React from "react";
+import { PageInfo as Props } from "@/pages/blog/[id]";
 import styles from "./index.module.scss";
-import { Tag, TagType } from "../Tag";
+import { Tag } from "../Tag";
 import { Writer } from "../Writer";
-
-type Props = {
-  title: string;
-  writerName: string;
-  writerImage: string;
-  tags: TagType[];
-  date: string;
-};
 
 export const ArticleTitle: React.FC<Props> = ({ title, writerName, writerImage, tags, date }) => {
   return (
     <div>
       <Writer writerName={writerName} writerImage={writerImage} />
-      <div className={styles.title}>{title}</div>
+      <h1 className={styles.title}>{title}</h1>
       <div className={styles.tagContainer}>
         {tags.map((tag) => (
           <Tag key={tag.id} name={tag.name} color={tag.color} isLink={tag.isLink} />
