44d0538c795c2a3192d682512532ea5ea032ae0b
ai8nik <ai8nik@gmail.com>
2024-05-21 22:03:18 +0900

    活動内容画面

--- src/screens/Activities/index.tsx
+++ src/screens/Activities/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { MdNavigateNext } from "react-icons/md";
 import { ActivitiesCarousel } from "@/features/ActivitiesCarousel";
 import { Calendar } from "@/ui/Calendar";
 import { Heading1 } from "@/ui/Heading1";
 import { Heading2 } from "@/ui/Heading2";
+import { IconLink } from "@/ui/IconLink";
 import { Timeline } from "@/ui/Timeline";
 import styles from "./index.module.scss";
+{
+  /* <meta
+  name="viewport"
+  content="width=device-width, initial-scale=1, minimum-scale=1, user-scalable=yes"
+></meta>; */
+}
 
 type Props = {};
 
@@ -13,20 +21,22 @@ export const ActivitiesScreen: React.FC<Props> = (props) => {
     <main>
       <div className="activities">
         <Heading1 enTitle="Activities" jaTitle="活動内容" />
-        <div className={styles.ActivitiesCarousel}>
-          <ActivitiesCarousel />
-        </div>
-        <div className={styles.top}>
-          <h3 className={styles.text_title}>対面×オンライン</h3>
-          <p className={styles.text}>
-            週に一度、水曜日か金曜日に対面活動をしています。もちろん、参加は自由です！
-            <br />
-            また、オンラインでの活動もあるので“予定があってなかなか対面活動にいけない...”という方も安心。
-            <br />
-            同じ開発チームの仲間たちと集まって作業したり、先輩にプログラミングを基礎から教えてもらったり・・・
-            <br />
-            jackの仲間たちと一緒に、「やりたいこと」を「やれるように」なってみませんか？
-          </p>
+        <div className={styles.hoge}>
+          <div className={styles.top}>
+            <h3 className={styles.text_title}>対面×オンライン</h3>
+            <p className={styles.text}>
+              週に一度、水曜日か金曜日に対面活動をしています。もちろん、参加は自由です！
+              <br />
+              また、オンラインでの活動もあるので“予定があってなかなか対面活動にいけない...”という方も安心。
+              <br />
+              同じ開発チームの仲間たちと集まって作業したり、先輩にプログラミングを基礎から教えてもらったり・・・
+              <br />
+              jackの仲間たちと一緒に、「やりたいこと」を「やれるように」なってみませんか？
+            </p>
+          </div>
+          <div className={styles.ActivitiesCarousel}>
+            <ActivitiesCarousel />
+          </div>
         </div>
         <div className={styles.event}>
           <div className={styles.Heading2}>
@@ -55,6 +65,9 @@ export const ActivitiesScreen: React.FC<Props> = (props) => {
               カレンダーに「対面活動」とある日のうち、都合がよい日程で遊びに来てください！
               <br />
             </p>
+            <div className={styles.button}>
+              <IconLink href="" text="サイト" icon={MdNavigateNext} size="s" openInNewTab />
+            </div>
           </div>
         </div>
       </div>
