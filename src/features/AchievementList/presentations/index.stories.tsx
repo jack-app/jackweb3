dac45bc0a7df000e42baa1bb046599dec4c5a67b
shioneko <shioneko37@gmail.com>
2024-05-22 22:33:11 +0900

    レビュー修正　#52

--- src/features/AchievementList/presentations/index.stories.tsx
+++ src/features/AchievementList/presentations/index.stories.tsx
@@ -1,7 +1,78 @@
+import { text } from "stream/consumers";
+import id from "@fullcalendar/core/locales/id.js";
+import image from "next/image";
 import type { Meta, StoryObj } from "@storybook/react";
 import { AchievementListPresentation } from "./";
+import { Props as AchievementItemProps } from "@/ui/AchievementItem";
 
 type T = typeof AchievementListPresentation;
+
+const sampleYears = ["2024", "2023", "2022"];
+
+type Achievement = {
+  id: string;
+  date: string;
+  text: string;
+  image: string;
+  article_href: string;
+  web_href: string;
+  app_href: string;
+  google_href: string;
+  git_href: string;
+};
+
+const sampleAchievements: Record<string, Achievement[]> = {
+  "2024": [
+    {
+      id: "2024",
+      date: "2024-06-02",
+      text: "test1",
+      image: "https://placehold.jp/200x200.png",
+      article_href: "https://ejje.weblio.jp/content/test",
+      web_href: "https://www.youtube.com/",
+      app_href: " https://www.youtube.com/",
+      google_href: "https://www.youtube.com/",
+      git_href: "https://github.com/jack-app/jackweb3/issues/45",
+    },
+    {
+      id: "2024",
+      date: "2024-05-20",
+      text: "testtesttesttesttesttesttesttest4",
+      image: "",
+      article_href: "",
+      web_href: "",
+      app_href: "",
+      google_href: "",
+      git_href: "",
+    },
+  ],
+  "2023": [
+    {
+      id: "2023",
+      date: "2023-05-15",
+      text: "test2",
+      image: "https://placehold.jp/200x200.png",
+      article_href: "https://ejje.weblio.jp/content/test",
+      web_href: "",
+      app_href: " ",
+      google_href: "",
+      git_href: "",
+    },
+  ],
+  "2022": [
+    {
+      id: "2022",
+      date: "2022-05-14",
+      text: "test3",
+      image: "",
+      article_href: "",
+      web_href: "",
+      app_href: " ",
+      google_href: "https://www.youtube.com/",
+      git_href: "https://github.com/jack-app/jackweb3/issues/45",
+    },
+  ],
+};
 
 const meta: Meta<T> = {
   title: "features/AchievementList",
@@ -10,12 +81,18 @@ const meta: Meta<T> = {
     layout: "centered",
   },
   tags: ["autodocs"],
-  argTypes: {},
+  argTypes: {
+    sortedYear: { control: sampleYears },
+    groupedAchievement: { control: sampleAchievements },
+  },
 };
 
 export default meta;
 type Story = StoryObj<T>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    sortedYear: sampleYears,
+    groupedAchievement: sampleAchievements,
+  },
 };
--- src/screens/Achievements/index.tsx
+++ src/screens/Achievements/index.tsx
@@ -3,17 +3,11 @@ import styles from "./index.module.scss";
 import { AchievementList } from "@/features/AchievementList";
 import { Props as AchievementItemProps } from "@/ui/AchievementItem";
 import { Heading1 } from "@/ui/Heading1";
-import { groupBy } from "@/utils/groupBy";
 
 type Props = {
   achievements: AchievementItemProps[];
 };
 export const AchievementsScreen: React.FC<Props> = ({ achievements }) => {
-  const groupedAchievement = groupBy(achievements, (achievement) => {
-    return new Date(achievement.date).getFullYear().toString();
-  });
-
-  const sortedYear = Object.keys(groupedAchievement).sort((a, b) => (a > b ? -1 : 1));
   return (
     <main>
       <Heading1 enTitle="Achievements" jaTitle="活動実績" />
