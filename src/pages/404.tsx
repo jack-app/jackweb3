184c3996af2cff51203f9e8837aa206212acc4e2
yutteee <yusaku_nakamura_aa@icloud.com>
2024-06-07 22:59:04 +0900

    レイアウト調整

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import styles from "./index.module.scss";
 export default function Custom404() {
   return (
-    <Image
-      className={styles.img404}
-      src="/404.png"
-      alt="お探しのページが見つかりません"
-      width={1280 + 160}
-      height={300}
-    />
+    <main className={styles.container}>
+      <Image
+        className={styles.img404}
+        src="/404.png"
+        alt="お探しのページが見つかりません"
+        width={1280 + 160}
+        height={300}
+      />
+    </main>
   );
 }
