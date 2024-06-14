8bdc69c24d9528c0559d0ee9e8142e219c3e8d63
ai8nik <ai8nik@gmail.com>
2024-06-14 18:36:07 +0900

    全体のレイアウト指定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import "@/styles/globals.scss";
-import type { AppProps } from "next/app";
 import { Footer } from "@/ui/Footer";
 import { Header } from "@/ui/Header";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
+      <div className="Header">
+        <Header />
+      </div>
       <Component {...pageProps} />
       <Footer />
     </>
