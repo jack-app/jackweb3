d3b23b8f54e7bc85975901628dcd85c1b9b8eeef
Sattsunn <satsusarii@gmail.com>
2024-06-26 20:05:57 +0900

    fix long code

--- src/ui/Production/logics/index.tsx
+++ src/ui/Production/logics/index.tsx
@@ -3,41 +3,24 @@ import { RichText } from "@/types/block";
 import styles from "./../index.module.scss";
 
 export const RenderText = (text: RichText[]) => {
+  const [isExpanded, setIsExpanded] = useState(false);
   const toggleExpand = () => {
     setIsExpanded(!isExpanded);
   };
 
-  const [isExpanded, setIsExpanded] = useState(false);
-
   // textの正規化
-  let textAsString = "";
-  text.map((value: RichText) => {
-    const { text } = value;
-    if (!text) return null;
-    textAsString += text.content;
-  });
+  const textAsString = text.reduce((acc, { text }) => (text ? acc + text.content : acc), "");
 
-  if (!isExpanded) {
-    // 展開されていない場合、最初の70文字と「もっと見る」ボタンを表示
-    return (
-      <>
-        {textAsString.length > 70 ? textAsString.substring(0, 70) + "　..." : textAsString}
-        {textAsString.length > 70 && (
-          <button onClick={toggleExpand} className={styles.moreButton}>
-            もっと見る
-          </button>
-        )}
-      </>
-    );
-  } else {
-    // 展開されている場合、全テキストと「閉じる」ボタンを表示
-    return (
-      <>
-        {textAsString}
+  return (
+    <>
+      {textAsString.length > 70 && !isExpanded
+        ? `${textAsString.substring(0, 70)}　...`
+        : textAsString}
+      {textAsString.length > 70 && (
         <button onClick={toggleExpand} className={styles.moreButton}>
-          閉じる
+          {isExpanded ? "閉じる" : "もっと見る"}
         </button>
-      </>
-    );
-  }
+      )}
+    </>
+  );
 };
