4066aedab9b73c1a65a9345ce70966dd59e4a7ba
kotttto <nanasan770@outlook.com>
2024-12-16 14:23:18 +0900

    blogのWriterからnext/imageを引き剝がした。 #134

--- src/ui/Writer/index.tsx
+++ src/ui/Writer/index.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image"; // Import the Image component from next/image
 import Link from "next/link";
 import React from "react";
 import styles from "./index.module.scss";
@@ -13,13 +12,13 @@ export const Writer: React.FC<Props> = ({ writerName, writerImage, isLink = fals
   return isLink ? (
     <Link href={`/blog/writer/${writerName}`}>
       <div className={styles.link}>
-        <Image className={styles.image} src={writerImage} alt={writerName} width={24} height={24} />
+        <img className={styles.image} src={writerImage} alt={writerName} width={24} height={24} />
         <span className={styles.name}>{writerName}</span>
       </div>
     </Link>
   ) : (
     <div className={styles.writer}>
-      <Image className={styles.image} src={writerImage} alt={writerName} width={24} height={24} />
+      <img className={styles.image} src={writerImage} alt={writerName} width={24} height={24} />
       <span className={styles.name}>{writerName}</span>
     </div>
   );
