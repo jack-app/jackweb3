df16d2131e18f3fef54389d2a8e4ed5e03e894e7
kotttto <nanasan770@outlook.com>
2026-03-11 19:16:02 +0900

    feat: 画像表示コンポーネントを改善し、Next.jsのImageコンポーネントを使用

--- src/features/BlogArticleBodies/presentations/blocks/image/index.tsx
+++ src/features/BlogArticleBodies/presentations/blocks/image/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Image as ImageBlock } from "@/types/block";
 
 type Props = {
@@ -8,18 +9,21 @@ type Props = {
 
 export const ImagePresentation: React.FC<Props> = ({ image, id, pageId }) => {
   if (!image) return null;
-  let src = "";
-  image.type === "external" && image.external && (src = image.external.url);
-  image.type === "file" && image.file && (src = image.file?.url);
+
+  const src = image.type === "external" ? image.external?.url : image.file?.url;
+  if (!src) return null;
 
   const caption = image.caption ? image.caption[0]?.plain_text : "";
   return (
-    <figure className="mx-auto">
-      {image.type === "external" ? (
-        <img src={src} alt={caption} width={500} height={500} />
-      ) : (
-        <img src={src} alt={caption} width={500} height={500} />
-      )}
+    <figure className="mx-auto m-8 w-full  max-w-[800px]">
+      <Image
+        src={src}
+        alt={caption}
+        width={1200}
+        height={1200}
+        sizes="(max-width: 1023px) 100vw, 800px"
+        style={{ width: "100%", height: "auto" }}
+      />
       {caption && <figcaption>{caption}</figcaption>}
     </figure>
   );
