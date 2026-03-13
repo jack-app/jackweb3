a9b38bbdbe8ec606a7880cc8f13202891e88f90c
kotttto <nanasan770@outlook.com>
2026-03-13 16:47:52 +0900

    feat: 画像表示コンポーネントのスタイルを改善し、アスペクト比を設定

--- src/features/BlogArticleBodies/presentations/blocks/image/index.tsx
+++ src/features/BlogArticleBodies/presentations/blocks/image/index.tsx
@@ -14,16 +14,18 @@ export const ImagePresentation: React.FC<Props> = ({ image, id, pageId }) => {
   const src = image.type === "external" ? image.external?.url : image.file?.url;
   if (!src) return null;
 
+  const width = image.width;
+  const height = image.height;
+
   const caption = image.caption ? image.caption[0]?.plain_text : "";
   return (
     <figure className={styles.figure}>
-      <div className={styles.imageWrapper}>
+      <div className={styles.imageWrapper} style={{ aspectRatio: `${width} / ${height}` }}>
         <Image
           className={styles.image}
           src={src}
           alt={caption}
-          width={1200}
-          height={1200}
+          fill
           sizes="(max-width: 1023px) 100vw, 800px"
         />
       </div>
