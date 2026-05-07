9593f1d0c78013798b0a242f87570131d6e6759e
kotttto <nanasan770@outlook.com>
2026-05-07 13:32:01 +0900

    refactor: writerImageプロパティを削除し、引数を整理

--- src/ui/Writer/index.stories.tsx
+++ src/ui/Writer/index.stories.tsx
@@ -12,7 +12,6 @@ const meta: Meta<T> = {
   tags: ["autodocs"],
   argTypes: {
     writerName: { control: "text" },
-    writerImage: { control: "text" },
     isLink: { control: "boolean" },
   },
 };
@@ -23,8 +22,6 @@ type Story = StoryObj<T>;
 export const Default: Story = {
   args: {
     writerName: "さっつん",
-    writerImage:
-      "https://s3-us-west-2.amazonaws.com/public.notion-static.com/6f332397-5c6b-445e-8d47-6e9192e786e8/IMG_0346.jpg",
     isLink: false,
   },
 };
